6b6547bc91afa53ecd3ff2669ddcdc5b68fe3877
8232 <45120883+8232@users.noreply.github.com>
2018-11-22 09:10:17 +0530

    added better comments

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/profile/<name>")
 def profile(name):
     return render_template("profile.html", name=name)
-
+#this is not what you think
 
 if __name__ == "__main__":
     app.debug = True
